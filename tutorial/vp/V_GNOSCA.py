871b8e17e6e0f2f8cf4486ad1350bd0c989883bd
esgn <emmanuel.seguin@ign.fr>
2021-01-24 13:43:13 +0100

    version 2.3.3

--- tutorial/vp/V_GNOSCA.py
+++
@@ -1,5 +0,0 @@
-from istsoslib.responders.GOresponse import VirtualProcessHQ
-class istvp(VirtualProcessHQ):
-    procedures = {
-        "RH_GNOSCA": "urn:ogc:def:parameter:x-istsos:1.0:river:water:height"
-    }
